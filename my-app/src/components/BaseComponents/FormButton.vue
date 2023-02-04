33ae8f933ceca42b2a537895f92efe75d46e2f41
Umut Erol <umut_erol1999@hotmail.com>
2023-02-04 16:08:04 +0300

    fix: button conditional css

--- my-app/src/components/BaseComponents/FormButton.vue
+++ my-app/src/components/BaseComponents/FormButton.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="flex items-center justify-center">
     <button
-      class=" my-4 bg-[#8CA632] border-2 border-green-600 rounded-[3px] text-white"
+      :class="['my-4', 'border-2', 'border-[#8CA632]','rounded-[3px]', 'text-white',
+               {'bg-[#8CA632]': selectedTab === 'login', 'bg-[#E84362]' : selectedTab === 'register','border-[#E84362]': selectedTab === 'register'}]"
       style="width: 774px; height: 46px;"
-    > <p :class="['text-center', 'font-hind', 'font-normal', 'leading-tight','text-white']">
-      Giriş Yap
-    </p>
+    > <p v-if='selectedTab ==="login"' :class="['text-center', 'font-hind', 'font-normal', 'leading-tight','text-white']">
+        Giriş Yap
+      </p>
+      <p v-else :class="['text-center', 'font-hind', 'font-normal', 'leading-tight','text-white']">
+        Üye Ol
+      </p>
     </button>
   </div>
 </template>
 <script>
 export default {
+  props: {
+    selectedTab: {
+      type: String,
+      default: 'login',
+    },
+  },
   data() {
     return {
     };
