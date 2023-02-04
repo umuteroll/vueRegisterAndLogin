8a6d3b6230c9f8c98db172e941fa93a03d2530b3
Umut Erol <umut_erol1999@hotmail.com>
2023-02-04 16:11:55 +0300

    fix: bind input with emit and props added

--- my-app/src/components/BaseComponents/FormInput.vue
+++ my-app/src/components/BaseComponents/FormInput.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="flex items-center justify-center">
     <input
-      :value="value"
-      class='w-full mx-5 my-2 bg-transparent border-b-[1px] rounded-[3px]
-       border-[#bdbdc0] focus:outline-none py-2 text-[#FDFEFD] placeholder-[#FDFEFD] font-hind font-normal'
-      placeholder="E- Posta Adresiniz"
-    >
+      :value="modelValue"
+      class="
+        w-full mx-5 my-2 bg-transparent border-b-[1px] rounded-[3px] border-[#bdbdc0] focus:outline-none py-2
+         text-[#FDFEFD] placeholder-[#FDFEFD] font-hind font-normal "
+      :placeholder="holder"
+      @input="updateValue"
+    />
   </div>
 </template>
 <script>
 export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    holder: {
+      type: String,
+      default: '',
+    },
+  },
   data() {
-    return {
-      value: 'selam',
-    };
+    return {};
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('update:modelValue', event.target.value);
+    },
   },
 };
 </script>
