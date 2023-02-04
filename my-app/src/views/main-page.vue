26b53b89b31a24d322631e139faf5202d9c1107d
Umut Erol <umut_erol1999@hotmail.com>
2023-02-04 03:38:59 +0300

    feat: main page created

---
+++ my-app/src/views/main-page.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-screen w-screen bg-[#2E323E] flex justify-center items-center">
+    <Form></Form>
+  </div>
+</template>
+<script>
+import Form from '../components/Form.vue';
+export default {
+  components: {
+    Form,
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  async mounted() {
+  },
+  methods: {
+  },
+};
+</script>
