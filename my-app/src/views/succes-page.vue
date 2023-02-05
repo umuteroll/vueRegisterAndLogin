c4af5b73e76943e4e7b43f79a2901d245905338a
Umut Erol <umut_erol1999@hotmail.com>
2023-02-05 22:42:00 +0300

    feat: succes page created

---
+++ my-app/src/views/succes-page.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-screen w-screen bg-[#2E323E] flex justify-center items-center text-white">
+    BAŞARILI GİRİŞ
+  </div>
+</template>
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  async mounted() {
+  },
+  methods: {
+  },
+};
+</script>
